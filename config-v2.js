aef345c3ef3f01d4875c33425fbee65e45ce65e0
Chethmina Kavishan <atadeka999@gmail.com>
2024-09-16 10:24:29 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
 const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const BOT_NUMBER = "94779510013"
+const SESSION_ID = "PRABATH-MD~POIRwJCR#Po902kC_-Q6H_P3mvdSTup0QEqXhidyLeV-Z2gDO1-c"
 
 
 module.exports = {
